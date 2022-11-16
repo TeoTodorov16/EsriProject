ec640d87467843c2ff1ddcafb71ea45324a12c0c
Teodor Todorov <WorkCoding@Teodors-MacBook-Pro.local>
2022-11-16 15:06:59 +0200

    Commit to GitHub finished App for Esri

---
+++ src/App.jsx
@@ -0,0 +1,157 @@
+import React, {useEffect, useRef, useState} from 'react'
+import './App.css'
+import { Search, GpsFixed } from "@material-ui/icons"
+
+
+const apiKey = import.meta.env.VITE_APP_GMAP_API_KEY;
+const mapApiJs = 'https://maps.googleapis.com/maps/api/js';
+const geocodeJson = 'https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer';
+
+
+// load map api js
+
+function loadAsyncScript(src) {
+  return new Promise(resolve => {
+    const script = document.createElement("script");
+    Object.assign(script, {
+      type: "text/javascript",
+      async: true,
+      src
+    })
+    script.addEventListener("load", () => resolve(script));
+    document.head.appendChild(script);
+  })
+}
+
+const extractAddress = (place) => {
+
+  const address = {
+    city: "",
+    state: "",
+    zip: "",
+    country: "",
+    plain() {
+      const city = this.city ? this.city + ", " : "";
+      const zip = this.zip ? this.zip + ", " : "";
+      const state = this.state ? this.state + ", " : "";
+      return city + zip + state + this.country;
+    }
+  }
+
+  if (!Array.isArray(place?.address_components)) {
+    return address;
+  }
+
+  place.address_components.forEach(component => {
+    const types = component.types;
+    const value = component.long_name;
+
+    if (types.includes("locality")) {
+      address.city = value;
+    }
+
+    if (types.includes("administrative_area_level_2")) {
+      address.state = value;
+    }
+
+    if (types.includes("postal_code")) {
+      address.zip = value;
+    }
+
+    if (types.includes("country")) {
+      address.country = value;
+    }
+
+  });
+
+  return address;
+}
+
+
+function App() {
+
+  const searchInput = useRef(null);
+  const [address, setAddress] = useState({});
+
+
+  // init gmap script
+  const initMapScript = () => {
+    // if script already loaded
+    if(window.google) {
+      return Promise.resolve();
+    }
+    const src = `${mapApiJs}?key=${apiKey}&libraries=places&v=weekly`;
+    return loadAsyncScript(src);
+  }
+
+  // do something on address change
+  const onChangeAddress = (autocomplete) => {
+    const place = autocomplete.getPlace();
+    setAddress(extractAddress(place));
+  }
+
+  // init autocomplete
+  const initAutocomplete = () => {
+    if (!searchInput.current) return;
+
+    const autocomplete = new window.google.maps.places.Autocomplete(searchInput.current);
+    autocomplete.setFields(["address_component", "geometry"]);
+    autocomplete.addListener("place_changed", () => onChangeAddress(autocomplete));
+
+  }
+
+
+  const reverseGeocode = ({ latitude: lat, longitude: lng}) => {
+    const url = `${geocodeJson}?key=${apiKey}&latlng=${lat},${lng}`;
+    searchInput.current.value = "Getting your location...";
+    fetch(url)
+        .then(response => response.json())
+        .then(location => {
+          const place = location.results[0];
+          const _address = extractAddress(place);
+          setAddress(_address);
+          searchInput.current.value = _address.plain();
+        })
+  }
+
+
+  const findMyLocation = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(position => {
+        reverseGeocode(position.coords)
+      })
+    }
+  }
+
+
+
+
+
+  useEffect(() => {
+    initMapScript().then(() => initAutocomplete())
+  }, []);
+
+
+
+  return (
+    <div className="App">
+      <div>
+        <div className="search">
+          <span><Search /></span>
+          <input ref={searchInput} type="text" placeholder="Търсене на локация...."/>
+          <button onClick={findMyLocation}><GpsFixed /></button>
+        </div>
+
+        <div className="address">
+          <p>Град: <span>{address.city}</span></p>
+          <p>Държавен адрес: <span>{address.state}</span></p>
+          <p>Зип: <span>{address.zip}</span></p>
+          <p>Държава: <span>{address.country}</span></p>
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default App
